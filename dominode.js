aa63ae5ad78896b495be3a9bcd71215dc06dd65a
Dominic Tarr <dominic.tarr@gmail.com>
2012-04-16 18:18:12 +1200

    make dominode also readable

--- dominode.js
+++ dominode.js
@@ -6,6 +6,7 @@ function Dominode(selector, template) {
   var me = this
   stream.Stream.call(me)
   me.writable = true
+  me.readable = true
   me.selector = document.querySelector(selector)
   me.template = template
 }
@@ -18,9 +19,10 @@ Dominode.prototype.write = function (data) {
   range.selectNode(document.body)
   var text = range.createContextualFragment(compiled)
   this.selector.appendChild(text)
+  this.emit('data', text)
 }
 
-Dominode.prototype.end = function () {}
+Dominode.prototype.end = function () { this.emit('end') }
 
 function dominode(selector, template) { return new Dominode(selector, template) }
 
